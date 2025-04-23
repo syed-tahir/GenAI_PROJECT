e44590320993132263601f0d394310d3fdf4fb11
syed-tahir <syedmohammedtahir2580@gmail.com>
2025-04-23 15:17:13 +0530

    Update GenAI_Video_Summarizer.py

--- GenAI_Video_Summarizer.py
+++ GenAI_Video_Summarizer.py
@@ -85,8 +85,8 @@ def turn_into_story(summary):
     return model.invoke(prompt)
 
 # Streamlit UI
-st.title("📺 PragyanAI - YouTube/Uploaded Video Summarizer Using Groq LLM")
-st.image("PragyanAI_Transperent.png")
+st.title("📺 SYED_THE_KING - YouTube/Uploaded Video Summarizer Using Groq LLM")
+#st.image("PragyanAI_Transperent.png")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
